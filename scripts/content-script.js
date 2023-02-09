dea0c11a466fab14136e02e1bfa116d0d879e63e
chrissayon <christopher.l.sayon@gmail.com>
2023-02-09 23:47:02 +1100

    refactor screen code

--- scripts/content-script.js
+++ scripts/content-script.js
@@ -1,27 +1,5 @@
 function selectElement() {
-  var overlay = document.getElementById('selectElementOverlayExtension');
-
-  // TODO: Put into another function is used again elsewhere
-  // Add overlay if it doesn't exist
-  if (overlay == null) {
-    const element = document.body.firstChild;
-    const newOverlay = document.createElement('div');
-    newOverlay.id = 'selectElementOverlayExtension';
-
-    // Darken screen
-    newOverlay.style.backgroundColor = 'rgba(0, 0, 0, 0.5)';
-    newOverlay.style.width = `${window.innerWidth}px`;
-    newOverlay.style.height = `${window.innerHeight}px`;
-    newOverlay.style.zIndex = '9999999';
-    newOverlay.style.display = 'fixed';
-    newOverlay.style.position = 'fixed';
-    newOverlay.style.pointerEvents = 'none';
-    newOverlay.style.top = '0';
-    newOverlay.style.left = '0';
-
-    document.body.insertBefore(newOverlay, element);
-    overlay = document.getElementById('selectElementOverlayExtension');
-  }
+  darkenScreen();
 }
 
 chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
---
+++ scripts/screen.js
@@ -0,0 +1,24 @@
+function darkenScreen() {
+  var overlay = document.getElementById('selectElementOverlayExtension');
+
+  // Add overlay if it doesn't exist
+  if (overlay == null) {
+    const element = document.body.firstChild;
+    const newOverlay = document.createElement('div');
+    newOverlay.id = 'selectElementOverlayExtension';
+
+    // Darken screen
+    newOverlay.style.backgroundColor = 'rgba(0, 0, 0, 0.5)';
+    newOverlay.style.width = `${window.innerWidth}px`;
+    newOverlay.style.height = `${window.innerHeight}px`;
+    newOverlay.style.zIndex = '9999999';
+    newOverlay.style.display = 'fixed';
+    newOverlay.style.position = 'fixed';
+    newOverlay.style.pointerEvents = 'none';
+    newOverlay.style.top = '0';
+    newOverlay.style.left = '0';
+
+    document.body.insertBefore(newOverlay, element);
+    overlay = document.getElementById('selectElementOverlayExtension');
+  }
+}
