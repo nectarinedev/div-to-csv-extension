914291f227be43efd9cb3702c953373187f7a5b9
chrissayon <christopher.l.sayon@gmail.com>
2023-02-09 23:33:13 +1100

    message pass between extension and content

--- content.js
+++ content.js
@@ -1,7 +1,35 @@
-document.body.addEventListener('mouseover', function (e) {
-  e.target.style.backgroundColor = 'yellow';
-});
+function selectElement() {
+  var overlay = document.getElementById('selectElementOverlayExtension');
 
-document.body.addEventListener('mouseout', function (e) {
-  e.target.style.backgroundColor = '';
+  // TODO: Put into another function is used again elsewhere
+  // Add overlay if it doesn't exist
+  if (overlay == null) {
+    const element = document.body.firstChild;
+    const newOverlay = document.createElement('div');
+    newOverlay.id = 'selectElementOverlayExtension';
+
+    // Darken screen
+    newOverlay.style.backgroundColor = 'rgba(0, 0, 0, 0.5)';
+    newOverlay.style.width = `${window.innerWidth}px`;
+    newOverlay.style.height = `${window.innerHeight}px`;
+    newOverlay.style.zIndex = '9999999';
+    newOverlay.style.display = 'fixed';
+    newOverlay.style.position = 'fixed';
+    newOverlay.style.pointerEvents = 'none';
+    newOverlay.style.top = '0';
+    newOverlay.style.left = '0';
+
+    document.body.insertBefore(newOverlay, element);
+    overlay = document.getElementById('selectElementOverlayExtension');
+  }
+}
+
+chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
+  switch (request.type) {
+    case 'createOverlay':
+      selectElement();
+      sendResponse({ value: 'Success' });
+    default:
+      break;
+  }
 });
--- popup.js
+++ popup.js
@@ -4,56 +4,12 @@ async function getCurrentTab() {
   return tab;
 }
 
-// function selectElement(tabId) {
-//   document.body.style.backgroundColor = 'red';
-//   // chrome.tabs.sendMessage(
-//   //   tabId,
-//   //   { createOverlay: { width: '100px', height: '100px', innerHTML: 'Hello' } },
-//   //   function (response) {
-//   //     console.log(response);
-//   //   }
-//   // );
-
-//   // chrome.runtime.sendMessage({
-//   //   message: { createOverlay: { width: '100px', height: '100px', innerHTML: 'Hello' } },
-//   //   callback: (response) => {
-//   //     console.log(response);
-//   //   },
-//   // });
-// }
-
-function selectElement() {
-  var overlay = document.getElementById('selectElementOverlayExtension');
-
-  // TODO: Put into another function is used again elsewhere
-  // Add overlay if it doesn't exist
-  if (overlay == null) {
-    const element = document.body.firstChild;
-    const newOverlay = document.createElement('div');
-    newOverlay.id = 'selectElementOverlayExtension';
-
-    // Darken screen
-    newOverlay.style.backgroundColor = 'rgba(0, 0, 0, 0.5)';
-    newOverlay.style.width = `${window.innerWidth}px`;
-    newOverlay.style.height = `${window.innerHeight}px`;
-    newOverlay.style.zIndex = '9999999';
-    newOverlay.style.display = 'fixed';
-    newOverlay.style.position = 'fixed';
-    newOverlay.style.pointerEvents = 'none';
-    newOverlay.style.top = '0';
-    newOverlay.style.left = '0';
-
-    document.body.insertBefore(newOverlay, element);
-    overlay = document.getElementById('selectElementOverlayExtension');
-  }
-}
-
 document.getElementById('selectButton').addEventListener('click', async () => {
-  let tab = await getCurrentTab();
+  const tab = await getCurrentTab();
 
-  chrome.scripting.executeScript({
-    target: { tabId: tab.id },
-    // files: ['select.js'],
-    func: selectElement, //whatever function runs here is executed as content script
+  const response = await chrome.tabs.sendMessage(tab.id, {
+    type: 'createOverlay',
   });
+
+  console.log(response);
 });
