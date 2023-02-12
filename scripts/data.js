84f8140727a8dd54c3fb5d886a8df3c951d7687f
chrissayon <christopher.l.sayon@gmail.com>
2023-02-13 00:08:16 +1100

    add data-testid selector

--- scripts/data.js
+++ scripts/data.js
@@ -1,11 +1,15 @@
 function sendData(e) {
   // Identifier for whether the element has a specific id or css
   // we can look up later on the page
+  // TODO: Look at a way of optimizing at this seems to be case by case
   let selector = 'None';
   let selectorValue = 'None';
   if (e.target.id.length !== 0) {
     selector = 'Id';
     selectorValue = e.target.id;
+  } else if (e.target['data-testid'] !== undefined) {
+    selector = 'TestId';
+    selectorValue = e.target['data-testid'];
   } else if (e.target.className !== 0) {
     selector = 'Class';
     selectorValue = e.target.className;
